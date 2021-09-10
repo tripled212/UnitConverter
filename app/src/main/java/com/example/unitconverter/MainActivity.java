c219272aae300f80c1ef4f4672ca2dc2ffe4afa7
tripled212 <76831072+tripled212@users.noreply.github.com>
2021-09-11 03:47:57 +0800

    Added Class
    class for unit converter

--- app/src/main/java/com/example/unitconverter/MainActivity.java
+++ app/src/main/java/com/example/unitconverter/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.unitconverter;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,49 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CardView cvCurrency=(CardView) findViewById(R.id.cvCurrency);
+        CardView cvWeight=(CardView) findViewById(R.id.cvWeight);
+        CardView cvTime=(CardView) findViewById(R.id.cvTime);
+        CardView cvLength=(CardView) findViewById(R.id.cvLength);
+        CardView cvArea=(CardView) findViewById(R.id.cvArea);
+        CardView cvTemp=(CardView) findViewById(R.id.cvTemp);
+
+
+        cvCurrency.setOnClickListener(v -> {
+            Intent intent=new Intent (MainActivity.this, Currency.class);
+            startActivity(intent);
+        });
+
+        cvWeight.setOnClickListener(v -> {
+            Intent intent=new Intent (MainActivity.this, Weight.class);
+            startActivity(intent);
+        });
+
+
+        cvTime.setOnClickListener(v -> {
+            Intent intent=new Intent (MainActivity.this, Time.class);
+            startActivity(intent);
+        });
+
+
+        cvLength.setOnClickListener(v -> {
+            Intent intent=new Intent (MainActivity.this, Length.class);
+            startActivity(intent);
+        });
+
+
+        cvArea.setOnClickListener(v -> {
+            Intent intent=new Intent (MainActivity.this, Area.class);
+            startActivity(intent);
+        });
+
+
+        cvTemp.setOnClickListener(v -> {
+            Intent intent=new Intent (MainActivity.this, Temperature.class);
+            startActivity(intent);
+        });
     }
+
+
 }
